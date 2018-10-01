d87fff02727baf5a15743ed8fc20c906a57b1fc5
Jwlin <jwlin17@gmail.com>
2018-10-01 01:09:58 +0100

    Updating style and positioning

--- src/Profile.js
+++ src/Profile.js
@@ -7,12 +7,10 @@ export default class Profile extends Component {
     return (
       <Grid>
         <Row>
-          <Col xs={6} md={4} mdOffset={4}>
-            <Image src="/web-github-iso/profile_pic_pro.JPG" rounded responsive/>
+          <Col xs={6} md={3} xsOffset={3} mdOffset={2}>
+            <Image src="/profile_pic_pro.JPG" rounded responsive/>
           </Col>
-        </Row>
-        <Row>
-          <Col xs={8} md={8} mdOffset={2}>
+          <Col xs={10} md={5} xsOffset={1} mdOffset={0}>
             <h4 className="profile-content">Hey! I'm a 3rd year Data Science major with Computer Science focus and an Economics Minor at UC San Diego. I am excited to be the first class of undergraduate data scientists at my school.
             However, my curriculum actually is equal parts Data Science and Computer Science. I am interested in learning how to effectively integrate data with the software projects that I work on.
             Over the summer, I took classes on computer vision and machine learning in addition to learning how to build a full-stack web app. </h4>
@@ -20,7 +18,6 @@ export default class Profile extends Component {
           </Col>
         </Row>
       </Grid>
-
     );
   }
 }
